9931f0d1091f9945bb3fb68279b0d5cb9c3c7591
DanielMensor <danielmmsmensor@gmail.com>
2023-04-04 11:08:12 -0300

    Criado exercicio 3

--- estruturas-de-repetição/src/Exercicio3.java
+++ estruturas-de-repetição/src/Exercicio3.java
@@ -1,3 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Exercíco Pedindo 5 numero e informando a média deles e o maior numero informado;
+ * 
+ * @author Daniel Mensor;
+ */
+
 public class Exercicio3 {
     
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+        double numero;
+        double maior=0;
+        double soma=0;
+
+
+        int count=0;
+        do{
+            System.out.println("numero:");
+            numero = scan.nextDouble();
+
+            if(numero> maior){
+                maior = numero;
+            }
+
+            soma +=numero;
+            count++;
+        }while(count<5);
+
+        System.out.println("maior numero informado : " + maior);
+        System.out.println("média dos 5 numeros:" + (soma/5));
+
+        scan.close();
+
+    }
 }
