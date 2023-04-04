17028f6b7bff96ea9cf84236c252627086ee3e58
DanielMensor <danielmmsmensor@gmail.com>
2023-04-04 10:49:04 -0300

    Criado primeiros 2 exercicios de loop

---
+++ estruturas-de-repetição/src/Exercicio1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner s = new Scanner(System.in);
+        
+        String nome;
+        int idade;
+
+        while(true){
+            
+            System.out.println("Qual o nome do Aluno? ");
+            nome = s.next();
+            if(nome.equals("0"))  break;
+    
+            System.out.println("Qual a idade do Aluno? ");
+            idade = s.nextInt();
+
+            System.out.println("nome do aluno: " + nome + ", Idade: " + idade);
+        }
+
+        System.out.println("Terminando");
+        s.close();
+    }
+}
---
+++ estruturas-de-repetição/src/Exercicio2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        double nota;
+
+        System.out.println("Informe uma nota entre 0 e 10");
+        nota = scan.nextDouble();
+
+        while(nota>10||nota<0){
+            System.out.println("Nota invalida informe novamente uma nota entre 0 e 10:");
+            nota = scan.nextDouble();
+        }
+        System.out.println("nota informada: " + nota);
+        scan.close();
+    }
+}
