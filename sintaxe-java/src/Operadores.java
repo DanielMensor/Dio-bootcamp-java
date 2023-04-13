fd8e0d96742baecee1c47b1e082b79c4219b0a65
DanielMensor <danielmmsmensor@gmail.com>
2023-04-13 14:33:48 -0300

    Mudanças pequenas de comentários

--- sintaxe-java/src/Operadores.java
+++ sintaxe-java/src/Operadores.java
@@ -1,17 +1,17 @@
 public class Operadores {
     public static void main(String[] args) {
-        boolean b = (5<7);/* 5 é menor que 7 retornando true*/
-        b = (5>7); /* 5 maior que 7 retornando false */
-        b = (5>=7); /*5 maior ou igual a 7 retornando false */
-        b = (5<=7); /*5 menor ou igual a 7 retornando true */
-        b = (5==7); /*5 igual a 7 retornando false */
-        b = (5!=7); /*5 diferente de 7 retornando true */
-
-
-        b = (5==7 && 3>1); /*5 igual a 7 e 3 maior que 1 retornando false */
-        b = (5==7 || 3>1); /*5 igual a 7 ou 3 maior que 1 retornando true */
-
-        b = (5>7 ? true : false); /* se 5 for maior que 7 retorna true se não retorna false */
-        
+//        boolean b = (5<7);/* 5 é menor que 7 retornando true*/
+//        b = (5>7); /* 5 maior que 7 retornando false */
+//        b = (5>=7); /*5 maior ou igual a 7 retornando false */
+//        b = (5<=7); /*5 menor ou igual a 7 retornando true */
+//        b = (5==7); /*5 igual a 7 retornando false */
+//        b = (5!=7); /*5 diferente de 7 retornando true */
+//
+//
+//        b = (5==7 && 3>1); /*5 igual a 7 e 3 maior que 1 retornando false */
+//        b = (5==7 || 3>1); /*5 igual a 7 ou 3 maior que 1 retornando true */
+//
+//        b = (5>7 ? true : false); /* se 5 for maior que 7 retorna true se não retorna false */
+//        
     }
 }
--- sintaxe-java/src/TiposVariaveis.java
+++ sintaxe-java/src/TiposVariaveis.java
@@ -1,15 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args){
     
-        byte b = 5; /*numero pequeno */
-        short s = 100; /*numero pequeno */
-        int i = 150; /* numero inteiro mais usado */
-        long l = 150000000000l; /*numero inteiro grande */
-        float f = 10.3f; /*ponto flutuante */ 
-        double d = 5.2; /*ponto flutuante mais usado */
-        char c = 'a'; /* um unico caracter */
-        boolean boo = true; /* verdadeiro o u falso (true||false) */
-        String str = "Olá Mundo"; /* String é uma clasee que representa conjunto de caracteres. */
-        
+//        byte b = 5; /*numero pequeno */
+//        short s = 100; /*numero pequeno */
+//        int i = 150; /* numero inteiro mais usado */
+//        long l = 150000000000l; /*numero inteiro grande */
+//        float f = 10.3f; /*ponto flutuante */ 
+//        double d = 5.2; /*ponto flutuante mais usado */
+//        char c = 'a'; /* um unico caracter */
+//        boolean boo = true; /* verdadeiro o u falso (true||false) */
+//        String str = "Olá Mundo"; /* String é uma clasee que representa conjunto de caracteres. */
+//        
     }
 }
